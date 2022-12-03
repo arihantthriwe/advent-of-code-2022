20f86aea9ab3f280feb5586e0a1b70d3382a924a
Arihant Jain <arihant.jain@thriwe.com>
2022-12-04 03:11:55 +0530

    (test): added functional tests for day-02&day-03, (fix): missing package day-01

---
+++ day-03/day-03_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			main()
+		})
+	}
+}
+
+func Test_secondPart(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			secondPart()
+		})
+	}
+}
